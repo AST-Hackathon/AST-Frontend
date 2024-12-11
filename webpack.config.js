c96755e9fed326f5bc82b0c27a05517d553202e9
lsoodik <ssonya2311@gmail.com>
2024-12-11 22:53:48 +0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack"); // Добавлено для работы с .env
+
+module.exports = {
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    new Dotenv(), // Подключение dotenv-webpack
+  ],
+  entry: "./src/js/index.js",
+  output: {
+    path: path.resolve(__dirname, "output"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        sideEffects: true,
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [`...`, new CssMinimizerPlugin()],
+  },
+  mode: "development",
+};
