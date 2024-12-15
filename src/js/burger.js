85c463067065ecb7daa7a71831085a032b28f9a6
qkkkatrina <ybandaryuk@mail.ru>
2024-12-15 19:42:51 +0500

    Новая страница Снежная Королева

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,13 +1,29 @@
-const menuBurger = document.getElementById("menuBurger");
-const menu = document.getElementById("menu");
-const menuClose = document.getElementById("menuClose");
+// const menuBurger = document.getElementById("menuBurger");
+// const menu = document.getElementById("menu");
+// const menuClose = document.getElementById("menuClose");
 
-menuBurger.addEventListener("click", () => {
-  menu.classList.add("menu-active");
-  menuBurger.style.display = "none";
-});
+// menuBurger.addEventListener("click", () => {
+//   menu.classList.add("menu-active");
+//   menuBurger.style.display = "none";
+// });
 
-menuClose.addEventListener("click", () => {
-  menu.classList.remove("menu-active");
-  menuBurger.style.display = "block";
-});
+// menuClose.addEventListener("click", () => {
+//   menu.classList.remove("menu-active");
+//   menuBurger.style.display = "block";
+// });
+
+const menuBurger = document.getElementById('menuBurger');
+        const menu = document.getElementById('menu');
+        const menuClose = document.getElementById('menuClose');
+
+        // Показать меню и скрыть надпись "МЕНЮ"
+        menuBurger.addEventListener('click', () => {
+            menu.classList.add('menu-active');
+            menuBurger.style.display = 'none';
+        });
+
+        // Закрыть меню и вернуть надпись "МЕНЮ"
+        menuClose.addEventListener('click', () => {
+            menu.classList.remove('menu-active');
+            menuBurger.style.display = 'block';
+        });
